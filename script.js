c8ec5dcd073d89c570b51d610d43ebcaaf87c2a5
nabihakhan6t4 <khannabiha923@gmail.com>
2024-07-03 17:33:10 +0300

    start snake game

---
+++ script.js
@@ -0,0 +1,24 @@
+// Game Constants & Variables
+
+let direction = { x: 0, y: 0 };
+let foodSound = new Audio("eat.mp3");
+let gameOverSound = new Audio("game_over.mp3");
+let moveSound = new Audio("snake_move.mp3");
+let musicSound = new Audio("snake_music(1).mp3");
+
+// Game Function
+function main(currentTime) {
+    window.requestAnimationFrame(main);
+}
+
+
+
+
+
+
+
+
+
+
+// Main Logic Starts Here
+window.requestAnimationFrame(main);
